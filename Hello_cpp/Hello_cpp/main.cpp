2fefc20920dc0ebae8dfa2921c1c5964f926352e
sis-shen <adongfa@gmail.com>
2023-12-03 20:40:15 +0800

    2

--- Hello_cpp/Hello_cpp/main.cpp
+++ Hello_cpp/Hello_cpp/main.cpp
@@ -2,9 +2,33 @@
 
 #include "Test.h"
 
+#define ADD(x,y) ((x)+(y))
+
+//КЏЪ§жиди
+int Add(int a, int b)
+{
+	return a + b;
+ }
+
+double Add(double a, double b)
+{
+	return a + b;
+}
+
+void print(char a, int b)
+{
+	std::cout << a << " " << b << std::endl;
+}
+
+void print(int a, char b)
+{
+	std::cout << a << " " << b << std::endl;
+}
+
+
 int main()
 {
 
-
+	test::add(1,2);
 	return 0;
 }
