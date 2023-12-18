cdeb0fea349ba3cbf57226bd235be27e9c80b639
sis-shen <adongfa@gmail.com>
2023-12-18 22:00:48 +0800

    44

---
+++ string/string/ssstring.cpp
@@ -0,0 +1,42 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//int main()
+//{
+//	string s1("hello world");
+//	s1 += "what";
+//	cout << s1 << endl;
+//
+//	string::iterator it = s1.begin();
+//
+//	while (it != s1.end())
+//	{
+//		cout << *it << " ";
+//		++it;
+//		
+//	}
+//	cout << "===" << endl;
+//
+//	for (auto ch : s1)
+//	{
+//		cout << ch << " ";
+//	}
+//	cout << endl;
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	string s1("hello world");
+	string::reverse_iterator it = s1.rbegin();//·´Ïòµü´úÆ÷
+	
+	auto it2 = s1.rbegin();.0
+
+	return 0;
+}
