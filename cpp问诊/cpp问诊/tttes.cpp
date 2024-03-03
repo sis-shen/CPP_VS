60851c5053f68e864e99a9c77a0da73d1cd012fb
sis-shen <adongfa@gmail.com>
2024-03-03 22:32:05 +0800

    1

--- cpp问诊/cpp问诊/tttes.cpp
+++ cpp问诊/cpp问诊/tttes.cpp
@@ -1,13 +1,44 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	for (int i = 1; i <= 2000; i++)
+//	{
+//		string a = to_string(i);
+//		cout << a << endl;
+//	}
+//
+//	return 0;
+//}
 
+#include<iostream>
+#include<cstring>
+#include<string>
+using namespace std;
 int main()
 {
-	
-
+	long long sum = 0;
+	for (int i = 1; i <= 2020; i++)
+	{
+		string a(to_string(i));
+		for (int j = 0; j < a.size(); j++)
+		{
+			//if(a[j] =='2' || a[j] == '0')
+			//cout << a[j] << endl;
+			char ch = a[j];
+			if (a[j] == '2' /*|| a[j] == '0' || a[j] == '1' || a[j] = '9'*/)
+			//if(ch =='2' || ch =='0')
+			{
+				sum += i * i;
+				break;
+			}
+		}
+	}
+	cout << sum << endl;
 	return 0;
 }
