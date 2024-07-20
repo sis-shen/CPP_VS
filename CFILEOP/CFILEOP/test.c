21d1bc3cd4e195479402d610dcae0cf20d2d12e1
sis-shen <adongfa@gmail.com>
2024-07-20 12:10:27 +0800

    1

--- CFILEOP/CFILEOP/test.c
+++ CFILEOP/CFILEOP/test.c
@@ -1,9 +1,15 @@
+
 #include <stdio.h>
 
 int main()
 {
-	FILE* f1 = fopen("file", "w");
-
-	fclose(f1);
+	FILE* pfile = fopen("file.txt", "w");//"w"模式打开文件file.txt
+	int code = 1;
+	const char* msg = "this is a msg";
+	fprintf(pfile, "get msg : %s code:%d", msg, code);
+	fclose(pfile);
+	pfile = fopen("file.txt", "a");
+	fprintf(pfile, "ADD");
+	fclose(pfile);
 	return 0;
 }
